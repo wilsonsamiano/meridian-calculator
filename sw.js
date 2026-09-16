a1ccc180efc014af589a82f4797c8fc38f862b93
Wilson Samiano <wilsonsamiano@users.noreply.github.com>
2026-09-16 20:58:40 +0000

    Fix iPadOS 27 top blur on the home-screen app

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE = "meridian-v2";
+const SCOPE = self.registration.scope;
+const PRECACHE = [
+  "./",
+  "./favicon.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+  "./manifest.webmanifest",
+  "./fonts/ibm-plex-sans-400.woff2",
+  "./fonts/ibm-plex-sans-500.woff2",
+  "./fonts/ibm-plex-sans-600.woff2",
+  "./fonts/ibm-plex-mono-400.woff2",
+  "./fonts/ibm-plex-mono-500.woff2",
+].map((path) => new URL(path, SCOPE).href);
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(PRECACHE).catch(() => undefined)),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))),
+    ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  if (url.pathname.includes("/api/") || url.pathname.includes("/__grok/")) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then((hit) => hit || caches.match(new URL("./", SCOPE))),
+      ),
+  );
+});
